9af7999c02147c128a0225ab3547739dbae90a50
kchandana832867-sketch <kchandana832867@gmail.com>
2026-01-31 15:06:35 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Test{
+    public static void main(String[] args)
+    {
+        Scanner ob=new Scanner(System.in);
+        System.out.println("enter the age of a person:"); 
+        int age=ob.nextInt();
+        System.out.println(age);
+    }
+}
