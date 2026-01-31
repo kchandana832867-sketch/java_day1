80e681846a635cbb30962a2bef5fc0ce790aa277
kchandana832867-sketch <kchandana832867@gmail.com>
2026-01-31 14:26:20 +0530

    Add files via upload

---
+++ demo.java
@@ -0,0 +1,9 @@
+ public class demo{
+    public static void main(String[] args)
+    {
+        System.out.print("hello world"); 
+        System.out.println("hi");
+        System.out.println("hello world"); 
+        System.out.println("hi");
+    }
+}
