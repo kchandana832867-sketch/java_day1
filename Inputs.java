a80a660d2d552468e7888805448d0f1aaadf0784
kchandana832867-sketch <kchandana832867@gmail.com>
2026-01-31 15:27:50 +0530

    Add files via upload

---
+++ Inputs.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Inputs{
+    public static void main(String[] args)
+    {
+        Scanner ob=new Scanner(System.in);
+        String e=ob.nextLine();
+        int age=ob.nextInt(); 
+        float b=ob.nextFloat();
+        double c=ob.nextDouble();
+        String d=ob.next();
+        System.out.println(age);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+
+    }
+}
